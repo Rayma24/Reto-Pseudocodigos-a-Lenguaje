5e3b8256bf687063a0d97bca3ad4016de1c5c409
Bkirving201 <86727411+Bkirving201@users.noreply.github.com>
2022-03-30 10:21:13 -0600

    Add files via upload

---
+++ Programa Contraseña .c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<windows.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int clave;
+	
+	printf("Programa de contraseña \n \n");
+	
+	do{
+		
+		printf("Dime tu contraseña de acceso :  ");
+		scanf("%i",&clave);
+		
+			if (clave != 897){
+				system("cls");
+				printf("Contraseña Incorrecta \n");
+			}
+			
+		
+	} while(clave!=897);
+	
+		system("cls");
+		printf(" ************************** \n");
+		printf(" Contraseña Correcta \n\n");
+		printf(" ************************** \n \n");
+		printf(" Bienvenido ");
+	
+	return 0;
+}
+
