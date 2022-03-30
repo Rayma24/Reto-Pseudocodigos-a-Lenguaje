f0c17082b1c96f5f701726aba0239c2b10408f53
Gabriel <86727180+ggarinc@users.noreply.github.com>
2022-03-30 10:23:15 -0600

    Pseudocodigo 10
    Programa que da una pista para ingresar una contraseña

---
+++ Password.py
@@ -0,0 +1,14 @@
+# Pseudocódigo de Arlet Jazmín Rodríguez Bibiano.
+password="404"
+
+cont=1
+
+greet=input("Ingresa la contraseña. Pista: Es el número de error que nos arroja un navegador cuando la página no existe. ")
+
+while password!=greet:
+
+          greet=input("Intenta nuevamente: ") 
+
+          cont=cont+1
+
+print("Bienvenido.")
